933c230cdd2161b2e2e05452d95d581b16cf0506
SebasED <johns.estrada@udea.edu.co>
2021-09-19 04:43:29 -0500

    corrections in schema of bd

--- src/Models/Propietario.js
+++ src/Models/Propietario.js
@@ -12,16 +12,42 @@ const PropietarioSchema = new Schema ({
     active: {
         type: Boolean,
         default: true
-    }
-    /*creation_date: { type: Date, default: Date.now },
-    update_date: { type: Date, default: Date.now },
-    registration_creator: String,
-    registry_updater: String,
-    number_id: Number,
-    cellphone: Number,
-    e_mail: String,
-    address: String,
-    pets:[{ name: String, id: Number }]*/
+    },
+    creation_date: { 
+        type: Date, 
+        default: Date.now, 
+        required: true
+    },
+    update_date: { 
+        type: Date, 
+        default: Date.now,
+        required: true 
+    },
+    registration_creator: {
+        type: String,
+        required: true
+    },
+    registry_updater: {
+        type: String,
+        required: true
+    },
+    cedula: {
+        type: Number,
+        required: true
+    },
+    cellphone: {
+        type: Number,
+        required: true
+    },
+    e_mail: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    pets:[{ name: String, id: Number }]
 });
 
 module.exports = model ("Propietario", PropietarioSchema)
